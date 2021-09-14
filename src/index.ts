222b52663ee3ac956e698bf057b6ed036d4bca09
alexander <aleksander9999a@gmail.com>
2021-09-14 08:26:59 +0300

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,76 @@
 import 'reflect-metadata';
 
-export interface DependencyConfig {
-  key: string,
-  type: 'multiton' | 'singleton'
-}
-
-export interface Dependency extends DependencyConfig {
-  instance: ObjectAny
-}
-
-export interface DependencyOptions {
-  type?: 'multiton' | 'singleton',
-  key?: string
-}
-
 export interface ObjectAny {
   [key: string]: any
 }
 
+interface ExDepOptions {
+  type?: 'multiton' | 'singleton',
+  key?: string
+}
+
+interface ExDepBase {
+  key: string,
+  type: 'multiton' | 'singleton'
+}
+
+interface ExDep extends ExDepBase {
+  ctr: ObjectAny
+  instance?: ObjectAny
+}
+
 export class ExContainer {
-  private container: Map<string, Dependency>;
+  private container: Map<string, ExDep>;
 
   constructor () {
     this.container = new Map();
     this.injectable = this.injectable.bind(this);
     this.resolve = this.resolve.bind(this);
+    this.bind = this.bind.bind(this);
     this.register = this.register.bind(this);
   }
 
-  injectable (options: DependencyOptions) {
+  injectable (options: ExDepOptions) {
     return (target: ObjectAny) => {
       Reflect.defineMetadata('ex-container:options', { key: target.name, type: 'multiton', ...options }, target.prototype);
       return target;
     }
   }
 
-  resolve (ctr: any) {
+  bind (ctr: any) {
     const metadata = (Reflect.getMetadata('design:paramtypes', ctr.prototype) || []).map(this.register);
-    return new ctr(...metadata);
+    return ctr.bind(ctr, ...metadata);
   }
 
-  register (ctr: ObjectAny) {
-    const options = Reflect.getMetadata('ex-container:options', ctr.prototype) as DependencyConfig|undefined;
+  resolve (ctr: any) {
+    return this.register(ctr);
+  }
+
+  private register (ctr: ObjectAny) {
+    const options = Reflect.getMetadata('ex-container:options', ctr.prototype) as ExDepBase|undefined;
 
     if (!options) {
       console.error(`Dependency is not decorated! ${JSON.stringify(ctr)}`);
-      return this.resolve(ctr);
     }
 
-    if (options.type === 'multiton') {
-      return this.resolve(ctr);
+    if (!options || options.type === 'multiton') {
+      const target = this.bind(ctr);
+      return new target();
     }
 
     let registeredTarget = this.container.get(options.key);
 
     if (!registeredTarget) {
+      const target = this.bind(ctr)
       registeredTarget = {
         ...options,
-        instance: this.resolve(ctr)
+        ctr: target,
+        instance: new target()
       }
 
       this.container.set(registeredTarget.key, registeredTarget);
     }
     
-    return registeredTarget.instance;
+    return registeredTarget.instance as ObjectAny;
   }
 }
